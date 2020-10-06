a1eb733ca76faf317b9d721f89ebbbfba70df958
iim-am <iim_mafahir@apps.ipb.ac.id>
2020-10-06 11:40:55 +0700

    Merubah dan menambbah file line 6 ,8 dan 9

--- helloworld.go
+++ helloworld.go
@@ -3,6 +3,8 @@ import "fmt"
 func main (){
 	fmt.Println("Hello world")
 
-	fmt.Println("Welcome to GIT")
+	fmt.Println("Welcome to Git")
 	fmt.Println("Tes merge")
+	fmt.Println("Mengenal Golang")
+	fmt.Println("Saya ingin bisa")
 }
