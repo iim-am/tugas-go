63f5faba14b774fc52000a97556fdaf47e1d82d3
iim-am <iim_mafahir@apps.ipb.ac.id>
2020-10-07 03:18:04 +0700

    membuat cabang dan menambahkan file di line 5,6,7,8

--- helloworld2.go
+++ helloworld2.go
@@ -2,4 +2,8 @@ package main
 import "fmt"
 func main (){
 	fmt.Println("Hello world")
+	fmt.Println("belajar git")
+	fmt.Println("belajar github dan gitlab")
+	fmt.Println("belajar go")
+	fmt.Println("belajar go")
 }
